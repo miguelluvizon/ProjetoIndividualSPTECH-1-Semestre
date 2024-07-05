60ec63a25b6588bef84a1c2bcaea9aca229c598d
miguelluvizon <miguel.luvizon@sptech.school>
2024-07-04 23:18:33 -0300

    Melhoria da tela de perfil e porcentagem de acertos do quiz

--- SITE INSTITUCIONAL/web-data-viz/public/js/sessao.js
+++ SITE INSTITUCIONAL/web-data-viz/public/js/sessao.js
@@ -4,6 +4,10 @@ function validarSessao() {
     var nome = sessionStorage.NOME_USUARIO;
     var idusuario = sessionStorage.ID_USUARIO;
 
+    var jogo_favorito = sessionStorage.JOGO_FAVORITO;
+    var jogo_estilo = sessionStorage.JOGO_ESTILO;
+    var jogo_plataforma = sessionStorage.JOGO_PLATAFORMA;
+
     var b_usuario = document.getElementById("b_usuario");
     
 
