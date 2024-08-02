e80f08d2c8b343b5835bdfb962781a9243abbf2f
miguelluvizon <miguel.luvizon@sptech.school>
2024-08-02 00:41:43 -0300

    nova página com uso de vetor(disputa)

--- SITE INSTITUCIONAL/web-data-viz/public/js/sessao.js
+++ SITE INSTITUCIONAL/web-data-viz/public/js/sessao.js
@@ -10,12 +10,15 @@ function validarSessao() {
 
     var b_usuario = document.getElementById("b_usuario");
     
+    
 
     if (email != null && nome != null) {
         b_usuario.innerHTML = nome;
     } else {
         window.location = "../login.html";
     }
+
+    
 }
 
 function limparSessao() {
@@ -23,20 +26,5 @@ function limparSessao() {
     window.location = "../login.html";
 }
 
-// carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-}
 
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
-
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
-    }
-}
 
